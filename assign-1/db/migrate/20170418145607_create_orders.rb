e3c64378c149a9307f7e4108419dd0250c77eac2
Leon <leon.karho.chung@gmail.com>
2017-04-18 11:01:57 -0400

    determined relationships

--- assign-1/db/migrate/20170418145607_create_orders.rb
+++ assign-1/db/migrate/20170418145607_create_orders.rb
@@ -1,8 +1,9 @@
 class CreateOrders < ActiveRecord::Migration[5.0]
   def change
     create_table :orders do |t|
-      t.string :order_number
-      t.string :date
+      t.string  :order_number
+      t.string  :date
+      t.integer :customers
 
       t.timestamps
     end
